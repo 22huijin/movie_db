d94ca31d3c4495ad5ce0871abb6bbbf4141c0c03
minji <gogohih@naver.com>
2025-04-30 13:05:17 +0900

    fix: 스웨거 어노테이션 수정

--- src/main/java/com/example/demo/movie/controller/ReviewController.java
+++ src/main/java/com/example/demo/movie/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.example.demo.movie.controller;
 import com.example.demo.movie.domain.Review;
 import com.example.demo.movie.dto.ReviewDTO;
 import com.example.demo.movie.service.ReviewService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reviews")
+@Tag(name = "리뷰 등록", description = "리뷰 등록 API")
 public class ReviewController {
 
     private final ReviewService reviewService;
