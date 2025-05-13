7914d4f0bbbf6c033d56889bc24e0b0dca2cc9b7
gogohih <gogohih@naver.com>
2025-05-13 21:00:02 +0900

    마이페이지 api 작성

--- src/main/java/com/example/demo/payment/domain/Payment.java
+++ src/main/java/com/example/demo/payment/domain/Payment.java
@@ -1,36 +1,32 @@
-//package com.example.demo.payment.domain;
-//
-//import com.example.demo.reservation.domain.Reservation;
-//<<<<<<< HEAD
-//import com.example.demo.coupon.domain.CouponUser;
-//=======
-////import com.example.demo.user.domain.CouponUser;
-//>>>>>>> b2ad39f649491d177a420eaeabef62595a771b35
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Table(name = "PAYMENT")
-//@Getter
-//@Setter
-//public class Payment {
-//
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long paymentId;
-//
-//  @OneToOne
-//  @JoinColumn(name = "reservation_id", nullable = false)
-//  private Reservation reservation;
-//
-//  private String paymentMethod;
-//
-//  @ManyToOne
-//  @JoinColumn(name = "coupon_user_id")
-//  private CouponUser couponUser;
-//
-//  private Integer finalPrice;
-//
-//  private String paymentStatus;
-//}
+package com.example.demo.payment.domain;
+
+import com.example.demo.reservation.domain.Reservation;
+import com.example.demo.coupon.domain.CouponUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PAYMENT")
+@Getter
+@Setter
+public class Payment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long paymentId;
+
+  @OneToOne
+  @JoinColumn(name = "reservation_id", nullable = false)
+  private Reservation reservation;
+
+  private String paymentMethod;
+
+  @ManyToOne
+  @JoinColumn(name = "coupon_user_id")
+  private CouponUser couponUser;
+
+  private Integer finalPrice;
+
+  private String paymentStatus;
+}
--- src/main/java/com/example/demo/payment/repository/PricingPolicyRepository.java
+++ src/main/java/com/example/demo/payment/repository/PricingPolicyRepository.java
@@ -1,7 +1,7 @@
-//package com.example.demo.payment.repository;
-//
-//import com.example.demo.payment.domain.PricingPolicy;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PricingPolicyRepository extends JpaRepository<PricingPolicy, Long> {
-//}
+package com.example.demo.payment.repository;
+
+import com.example.demo.payment.domain.PricingPolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PricingPolicyRepository extends JpaRepository<PricingPolicy, Long> {
+}
