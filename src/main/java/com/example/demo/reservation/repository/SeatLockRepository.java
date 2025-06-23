f8dc15432e45f4385ed355309b9ff0856644fd91
hijin <guu7898@gmail.com>
2025-06-23 17:26:08 +0900

    fix: 예약 취소 건에 대한 오류 처리

--- src/main/java/com/example/demo/reservation/repository/SeatLockRepository.java
+++ src/main/java/com/example/demo/reservation/repository/SeatLockRepository.java
@@ -2,7 +2,11 @@ package com.example.demo.reservation.repository;
 
 import com.example.demo.reservation.domain.SeatLock;
 import com.example.demo.schedule.domain.ScheduleSeat;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,4 +17,8 @@ public interface SeatLockRepository extends JpaRepository<SeatLock, Long> {
   List<SeatLock> findByScheduleSeatAndExpiresAtAfter(ScheduleSeat scheduleSeat, LocalDateTime now);
   void deleteByScheduleSeatAndExpiresAtBefore(ScheduleSeat scheduleSeat, LocalDateTime now);
   List<SeatLock> findByExpiresAtBefore(LocalDateTime now);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT sl FROM SeatLock sl WHERE sl.lockId IN :ids")
+  List<SeatLock> findAllByIdForUpdate(@Param("ids") List<Long> ids);
 }
